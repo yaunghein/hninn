4f71dbebe8cb8db6dfdff3f9a7c761bfbcbda9fa
Yan Aung Hein <yan@apmhelp.com>
2026-07-24 14:03:53 +0700

    menu page spacing and button cursor style

--- components/menu/header.tsx
+++ components/menu/header.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/common'
 
 export default function MenuHeader() {
   return (
-    <header className="flex flex-col items-center gap-13 px-6 py-18 xs:gap-10 xs:px-[12.5%] xs:pb-16 xs:pt-24">
+    <header className="flex flex-col items-center gap-13 px-6 py-18 xs:gap-10 xs:px-[12.5%] xs:pb-16 xs:pt-16">
       <h1 className="text-center text-[2rem] font-semibold uppercase leading-none tracking-[-0.02em] text-olive xs:text-5xl xs:tracking-tight">
         Menu
       </h1>
